88a1004cae805a3d923b0d3e0bcd27a90c477eb7
Перькова Кира <sk_perkova@mail.ru>
2026-03-16 22:21:25 +0300

    Add files via upload

---
+++ projects_3/impluse.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ $# -ne 2 ]; then
+    echo "Ошибка: не хватает аргументов. Нужно передать имя гена и уровень экспрессии."
+    exit 1
+fi 
+
+gene_name=$1
+expression_level=$2
+
+echo "Экспрессия гена $gene_name составляет $expression_level единиц"
